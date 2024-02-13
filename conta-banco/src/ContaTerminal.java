140027ef9111554f3d225e34652f19313c7ed3d7
Odisley <odisleyribeirosilva@hotmail.com>
2024-02-12 21:30:32 -0300

    Commit resolvido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ContaTerminal{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner sc = new Scanner(System.in);
+
+
+        //Exibir as mensagens para o nosso usuário
+
+        System.out.println("Por favor, digite o número da Agência !");
+        System.out.print("Usuário: ");
+        
+        //obter pela classe scanner os valores digitados pelo terminal
+        
+        int numero = sc.nextInt();
+        String nomeCliente = "Mario Andrade";
+        String agencia = "067-8";
+        double saldo = 237.48;
+
+        //Exibir a mensagem conta criada
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "  + numero +  " e seu saldo é  " + saldo +  " e já está disponível para saque!");
+        
+
+        
+
+       
+
+
+        
+
+
+        
     }
 }
